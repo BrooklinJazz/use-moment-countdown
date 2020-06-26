0451ab219d37a59b89b6cc3d61fbfa5ed21b9020
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-25 23:29:19 -0600

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import  {useCountdown} from 'use-moment-countdown'
+import { useCountdown } from "use-moment-countdown";
 
 const App = () => {
-  const {start, time} = useCountdown({m: 1})
+  const { start, time } = useCountdown({ m: 1 });
   return (
-    <div onClick={start}>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
       {time.format("mm:ss")}
+      <button style={{marginTop: 20}} onClick={start}>Start</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
