a120549ddf961c58e6758c27571875cbcb7064ab
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-25 22:14:09 -0600

    create use-countdown hook and install deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
-import * as React from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import moment from "moment";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+function useInterval(callback: () => any, delay: number) {
+  const savedCallback = useRef<() => any>(() => true);
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-  return counter;
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+
+export const useCountdown = (input: { m?: number; s?: number; h?: number }) => {
+  const { m = 0, s = 0, h = 0 } = input;
+  if (!m && !s && !h) {
+    throw Error(`useCountdown use be provided an input. you provided: ${input}`);
+  }
+  const intervalInMs = h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
+  const [count, setCount] = useState(0);
+  const diff = intervalInMs - count;
+  const remainingDuration = moment.duration(diff, "milliseconds");
+  const remainingMilliseconds = remainingDuration.asMilliseconds();
+
+  const [started, setStart] = useState(false);
+
+  useInterval(() => {
+    if (started && remainingMilliseconds !== 0) {
+      setCount(count + 1000);
+    }
+  }, 1000);
+
+  return {
+    time: moment.utc(remainingMilliseconds),
+    start: () => setStart(true),
+  };
 };
