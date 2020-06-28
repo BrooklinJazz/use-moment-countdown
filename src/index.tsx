186a92a8f38233beb130539d27703adb947781e9
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-27 20:08:57 -0600

    Improve types for useCountdown

--- src/index.tsx
+++ src/index.tsx
@@ -17,29 +17,41 @@ const useInterval = (callback: () => any, delay: number = 1000) => {
     let id = setInterval(tick, delay);
     return () => clearInterval(id);
   }, [delay]);
-}
+};
 
-export const useCountdown = (input: { m?: number; s?: number; h?: number }) => {
+type CountdownInput = { m?: number; s?: number; h?: number } | undefined;
 
+export const useCountdown = (
+  input: CountdownInput = {},
+  config: { onDone: () => any } = { onDone: () => true }
+) => {
   const { m = 0, s = 0, h = 0 } = input;
+  const { onDone } = config;
   const intervalInMs = h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000;
   const [count, setCount] = React.useState(0);
   const diff = intervalInMs - count;
   const remainingDuration = moment.duration(diff, "milliseconds");
   const remainingMilliseconds = remainingDuration.asMilliseconds();
 
-  const [started, setStart] = React.useState(false);
+  const [started, setStarted] = React.useState(false);
 
-  useInterval(() => {
-    if (started && remainingMilliseconds !== 0) {
-      setCount(count + 1000);
-    }
-  }, 1000);
+  useInterval(
+    () => {
+      if (started && remainingMilliseconds !== 0) {
+        setCount(count + 1000);
+      }
+      if (started && remainingMilliseconds === 0) {
+        setStarted(false);
+        onDone();
+      }
+    },
+    started ? intervalInMs : undefined
+  );
 
   return {
     time: moment.utc(remainingMilliseconds),
-    start: () => setStart(true),
-    stop: () => setStart(false),
-    started
+    start: () => setStarted(true),
+    stop: () => setStarted(false),
+    started,
   };
 };
