44c6ec8433421b04c23ac760c3299f6553e1f65f
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-27 22:46:25 -0600

    Update Examples

--- example/src/App.js
+++ example/src/App.js
@@ -2,22 +2,75 @@ import React from "react";
 
 import { useCountdown } from "use-moment-countdown";
 
+const Container = ({children}) => (
+  <div
+    style={{
+      height: 200,
+      width: 200,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      border: "solid 1px black",
+      marginBottom: 50
+    }}
+  >{children}</div>
+);
+
+const RecuringTimer = () => {
+  const { start, time, stop } = useCountdown(
+    { s: 10 },
+    {
+      recuring: true,
+      onDone: () => console.warn("You can pass an onDone function like this!"),
+    }
+  );
+  return (
+    <Container>
+      {time.format("mm:ss")}
+      <button style={{ marginTop: 20 }} onClick={start}>
+        Start Recuring Timer
+      </button>
+      <button style={{ marginTop: 20 }} onClick={stop}>
+        Stop
+      </button>
+    </Container>
+  );
+};
+
+const Timer = () => {
+  const { start, time, stop } = useCountdown(
+    { s: 10 },
+    {
+      onDone: () => console.warn("You can pass an onDone function like this!"),
+    }
+  );
+  return (
+    <Container>
+      {time.format("mm:ss")}
+      <button style={{ marginTop: 20 }} onClick={start}>
+        Start Regular Timer
+      </button>
+      <button style={{ marginTop: 20 }} onClick={stop}>
+        Stop
+      </button>
+    </Container>
+  );
+};
+
 const App = () => {
-  const { start, time, stop } = useCountdown({ s: 1 }, {recuring: true, onDone: () => console.warn("You can pass an onDone function like this!")});
   return (
     <div
       style={{
         height: "100vh",
         width: "100vw",
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-around",
         alignItems: "center",
-        flexDirection: "column"
       }}
     >
-      {time.format("mm:ss")}
-      <button style={{marginTop: 20}} onClick={start}>Start</button>
-      <button style={{marginTop: 20}} onClick={stop}>Stop</button>
+      <RecuringTimer />
+      <Timer />
     </div>
   );
 };
